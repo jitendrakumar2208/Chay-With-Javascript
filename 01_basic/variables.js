91c19ac06603205caafe3c87459b1b5a03584ef4
Jitendra Kumar <152462064+jitendrakumar2208@users.noreply.github.com>
2024-02-25 14:49:19 +0000

    1st lect done

---
+++ 01_basic/variables.js
@@ -0,0 +1,7 @@
+const accountId = 56453746
+let  accountEmail = "jk@google.com"
+var accountPassword = "jkpoojan@2208";
+
+//accountId = 89898;
+
+console.log(accountEmail,accountId,accountPassword);
